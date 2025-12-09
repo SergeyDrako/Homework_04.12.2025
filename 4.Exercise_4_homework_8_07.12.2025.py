f061cad09879ab9235c9430b6c19832f35eedebe
SergeyDrako <drakoserge1990@gmail.com>
2025-12-09 23:02:09 +0300

    new file

--- 4.Exercise_4_homework_8_07.12.2025.py
+++ 4.Exercise_4_homework_8_07.12.2025.py
@@ -2,24 +2,27 @@ import time
 def wait_with_retry_until(timeout, interval):
     def decorator (func):
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
+            func(*args, **kwargs)
             my_interval=0
             сounter=1
+
             while my_interval <= timeout:
-                if func(*args, **kwargs) is True:
+                result = func(*args, **kwargs)
+                if result is True: # еслі результат выполненія функціі True
                    print(f" Попытка {сounter}  успешная")
                    print('Элемент найден: ')
-                   break
+                   return result
                 else:
                     print(f" Попытка {сounter}  неуспешная")
                     сounter+=1
                     my_interval += interval
                     time.sleep(interval)
+            print("Время ожидания истекло. Элемент не найден.")
+            return None
         return wrapper
     return decorator
 
 @wait_with_retry_until(timeout=3, interval=0.5) # wait_with_retry_until=element_visible(timeout=3, interval=0.5)
 def element_visible():
-    return time.time() % 3 > 2  # имитация появления элемента
-
+    return time.time() % 3 > 2  # имитация появления элемента 0%3=остаток, 0.5%3 остаток 0.5, 1%3 остаток 1
 element_visible()
