915c4c045b039d03fa25ff079a0e2790d4f1e531
SergeyDrako <drakoserge1990@gmail.com>
2025-12-12 11:47:47 +0300

    Исправил второй вариант первого задания

--- 1.Exercise_1_homework_9_10.12.2025.py
+++ 1.Exercise_1_homework_9_10.12.2025.py
@@ -1,36 +1,34 @@
-class Vehicle:
-    def __init__(self, move):
-        self.move = int(move)
-    def move_1(self, perfect):
-        self.move = self.move + perfect
-        print(f"Машина едет со скорость {perfect}км/ч.")
-Vehicle_1 = Vehicle('50')
-Vehicle_1.move_1(30)
-print(f"Моя машина едет быстрее {Vehicle_1.move}км/ч.")
-
-class Car(Vehicle):
-    def honk(self):
-        print("БИП-БИП")
-car = Car(100)
-car.move_1(5)
-print(f"Самая быстрая машина на дороге:  {car.move}")
-car.honk()   # БИП-БИП!
-
 # class Vehicle:
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def move(self, move):
-#         print(f"{self.name} is moving.")
+#     def __init__(self, move):
+#         self.move = int(move)
+#     def move_1(self, perfect):
+#         self.move += perfect
+#         print(f"Машина едет со скорость {perfect}км/ч.")
+# Vehicle_1 = Vehicle('50')
+# Vehicle_1.move_1(30)
+# print(f"Моя машина едет быстрее {Vehicle_1.move}км/ч.")
 #
 # class Car(Vehicle):
 #     def honk(self):
-#         print("БИП-БИП!")
-#
-# Vehicle_1 = Vehicle()
-# Vehicle_1.move()
-#
+#         print("БИП-БИП")
 # car = Car(100)
-# car.move()
-# car.honk()
+# car.move_1(5)
+# print(f"Самая быстрая машина на дороге:  {car.move}")
+# car.honk()   # БИП-БИП!
+
+class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+    def move(self):
+        print(f"{self.name} is moving.")
+
+class Car(Vehicle):
+    def honk(self):
+        print("БИП-БИП!")
+
+car = Car(100)
+car.move()
+car.honk()
+
 
