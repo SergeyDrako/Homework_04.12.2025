715649532ee5792b4bddb140819e39243465901c
SergeyDrako <drakoserge1990@gmail.com>
2025-12-21 21:51:48 +0300

    delete_2

--- 2.1.Exercise_2.1_homework_7_04.12.2025.py
+++
@@ -1,8 +0,0 @@
-test_results = [
-    {"name": "login_test", "status": "passed", "duration": 2.1},
-    {"name": "payment_test", "status": "failed", "duration": 3.5},
-    {"name": "logout_test", "status": "passed", "duration": 1.2}
-]
-
-good_test = list(filter(lambda tests: tests["status"]=='passed',test_results))
-print(good_test)
