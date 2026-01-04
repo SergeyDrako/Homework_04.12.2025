f138d0bb00733131dda87b48a4a14c7f0286f2c3
SergeyDrako <drakoserge1990@gmail.com>
2026-01-04 23:52:32 +0300

    delete

--- 4. Exercise_4_homework_7_04.12.2025.py
+++
@@ -1,6 +0,0 @@
-def generate_report(title, *test_names, format="html", **options):
-    print('Отчет:',title)
-    print('Тесты: ', test_names)
-    print('Формат: ', format)
-    print('Разное:', options)
-generate_report("Daily Report", "test1", "test2", "test3", format="pdf", author="Tester", word="good")
